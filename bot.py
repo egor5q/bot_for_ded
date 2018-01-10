bbefcd43eae67659b105b638c07f7c62175a5e42
egor5q <31898567+egor5q@users.noreply.github.com>
2018-01-10 10:15:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,11 @@ def spamon(message):
     bot.send_message(id, message)
     
     
-
+@bot.message_handler(commands=['spam@dedspam_bot'])
+def no(m):
+ if m.from_user.id==185023717 or m.from_user.id==441399484:
+  bot.send_message(m.from_user.id, 'Не используйте эту форму команды! вводите её вручную!')
+                     
 @bot.message_handler(commands=['spam'])
 def spam(m):
   if m.from_user.id==185023717 or m.from_user.id==441399484:
@@ -30,14 +34,20 @@ def spam(m):
     z=len(m.text)
     message=m.text[5:z]
     if len(message)>0:
-    alreadyspam=1
-    spamon(message)
+      alreadyspam=1
+      spamon(message)
+    else:
+     bot.send_message(m.from_user.id, 'Введите что-нибудь после команды!')
+  else:
+   bot.send_message(m.chat.id, 'Только дед имеет право включать спам!')
     
 @bot.message_handler(commands=['off'])
 def off(m):
   if m.from_user.id==185023717 or m.from_user.id==441399484:
     global alreadyspam
     alreadyspam=0
+  else:
+   bot.send_message(m.chat.id, 'Только дед имеет право выключать спам!')
 
 @bot.message_handler(content_types=['text'])
 def add(m):
